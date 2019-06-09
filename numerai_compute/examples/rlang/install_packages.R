547a1d4785a1cf8d6ddd80133e901d0fc3cc864e
Jason Paryani <github@jparyani.com>
2019-06-09 12:03:34 -0700

    Install correct Rnumerai package

--- numerai_compute/examples/rlang/install_packages.R
+++ numerai_compute/examples/rlang/install_packages.R
@@ -1,2 +1,2 @@
-install.packages('Rnumerai', repo = 'https://cran.rstudio.com')
+devtools::install_github("Omni-Analytics-Group/Rnumerai")
 install.packages('pROC', repo = 'https://cran.rstudio.com')
