7f1fb65093aba76e2c0ea862442598b498805856
Jason Paryani <github@jparyani.com>
2019-06-09 12:04:25 -0700

    Bump to v0.1.12

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='numerai-cli',
-      version='0.1.11',
+      version='0.1.12',
       description='A library for helping to deploy a Numer.ai compute node',
       url='https://github.com/numerai/numerai-compute-cli',
       author='Numer.ai',
