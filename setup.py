55948b1a30ae728f4d73f4819e4e184d5772f081
Noah Harasz <ndharasz@live.com>
2021-01-15 14:16:01 -0500

    set version 0.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='numerai-cli',
-      version='0.2.2',
+      version='0.2.3',
       description='A library for helping to deploy a Numer.ai compute node',
       url='https://github.com/numerai/numerai-cli',
       author='Numer.ai',
