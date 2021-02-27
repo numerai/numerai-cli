80984a6d7938d0371fb579299cbeede3abc4dd0e
Noah Harasz <ndharasz@live.com>
2021-02-27 17:20:01 -0500

    generalize ubuntu setup script

--- scripts/setup-ubu.sh
+++ scripts/setup-ubu.sh
@@ -21,7 +21,9 @@
       gnupg-agent \
       software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
     apt update
     apt-cache policy docker-ce
     apt install -y docker-ce
