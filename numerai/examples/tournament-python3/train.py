95c16660626dcf56f94dc2d87361c0517ba5302c
Zoe Nolan <github@zoenolan.org>
2021-11-07 00:33:53 +0000

    Update the python 3 tournament example requirements

--- numerai/examples/tournament-python3/train.py
+++ numerai/examples/tournament-python3/train.py
@@ -4,5 +4,7 @@ import predict
 
 train_data_path, predict_data_path, predict_output_path = predict.download_data()
 
-for model_id, model_type in predict.MODEL_CONFIGS:
-    predict.train(train_data_path, model_id, model_type, force_training=True)
+model_id = predict.MODEL_ID
+model_type = predict.MODEL
+
+predict.train(train_data_path, model_id, model_type, force_training=True)
