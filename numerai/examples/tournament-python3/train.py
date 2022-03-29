7421ea1822928f733a018cc7ad3915b500daccb1
Noah Harasz <ndharasz@live.com>
2022-03-28 20:27:32 -0400

    Numerai CLI 0.3.4 (#44)
    - Improve formatting/docs to adhere to basic pylint conventions
    - Use `--no-cache-dir` during pip installs to prevent large docker image caches
    - Use `--platform linux/amd64` during docker builds to prevent errors (requires [Docker Engine API 1.3.8](https://docs.docker.com/engine/api/v1.38/) or higher)
    - Upgrade numpy from 1.20 -> 1.21 in signals example script
    - Upgrade all package versions in tournament example script
    - remove `profile = "default"` from -main.tf to prevent issues with awscli

--- numerai/examples/tournament-python3/train.py
+++ numerai/examples/tournament-python3/train.py
@@ -1,6 +1,10 @@
+""" An extra entry point specifically for training. Used when running locally """
+
 import predict
 
 train_data_path, predict_data_path, predict_output_path = predict.download_data()
 
-for model_id, model_type in predict.MODEL_CONFIGS:
-    predict.train(train_data_path, model_id, model_type, force_training=True)
+model_id = predict.MODEL_ID
+model_type = predict.MODEL
+
+predict.train(train_data_path, model_id, model_type, force_training=True)
